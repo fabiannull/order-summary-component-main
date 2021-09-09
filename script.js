3330868a32d2e6a249b905b9d419779ba211bc44
fabian <fabiannull@outlook.com>
2021-09-09 14:17:16 +0200

    fertig

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function alertboxfunction() {
-    alert("test");
+    alert("This is only a Demo from Frontend-Mentor to show my skills. If you like my work, feel free to contact me. You can contect me with the button at the bottom of this Page. Thank you.");
 }
